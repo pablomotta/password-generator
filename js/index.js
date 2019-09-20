64755f3c69fdc5c690dbf393cbf5baa79ce08318
Pablo <pmotta.dev@gmail.com>
2019-09-19 20:32:35 -0700

    password generator function working

--- js/index.js
+++ js/index.js
@@ -2,30 +2,18 @@
 
 //User can click 'generate password' button to generate new password
 
-//password needs to have:
-
-////special characters
-///////   !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~  (32 items)
-
-////numeric values
-///////   0123456789   (10 items)
-////lowercase characters
-///////   abcdefghijklmnopqrstuvwxyz  (26 items)
-////uppercase characters
-///////   ABCDEFGHIJKLMNOPQRSTUVWXYZ (26 items)
-
 //application should validate and ensure that at least one
 //character type of each group is selected
 
 //DOM targets
-var generateButton = document.getElementById('generate');
-var copyToClipboard = document.getElementById('copy');
+var generatePasswordBtn = document.getElementById('generate');
+var copyToClipboardBtn = document.getElementById('copy');
+var placeholderText = document.getElementById('password');
 
-//
-var numbers = '0123456789';
+// Password character types
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChar = [
     '!',
-    '"',
     '#',
     '$',
     '%',
@@ -34,7 +22,6 @@ var specialChar = [
     ')',
     '*',
     '+',
-    ',',
     '-',
     '.',
     '/',
@@ -49,7 +36,6 @@ var specialChar = [
     ']',
     '^',
     '_',
-    '`',
     '{',
     '|',
     '}',
@@ -111,52 +97,90 @@ var lowercase = [
     'y',
     'z'
 ];
-// console.log(Math.ceil(Math.random() * numbers.length + 2));
 
-// loop through array
+//character types random number:
 
 function randomNumGenerator(num) {
     return Math.ceil(Math.random() * num.length + 2);
 }
 
 function passwordGenerator(type1, type2, type3, type4) {
-    var randomNumType1 = randomNumGenerator(type1);
-    var randomNumType2 = randomNumGenerator(type2);
-    var randomNumType3 = randomNumGenerator(type3);
-    var randomNumType4 = randomNumGenerator(type4);
+    var randomIteration1 = randomNumGenerator(type1);
+    var randomIteration2 = randomNumGenerator(type2);
+    var randomIteration3 = randomNumGenerator(type3);
+    var randomIteration4 = randomNumGenerator(type4);
 
     var suggestedPassword = [];
 
-    for (var i = 0; i < randomNumType1; i++) {
-        suggestedPassword.push('1');
+    for (var i = 0; i < randomIteration1; i++) {
+        var numberRandomIndex = Math.floor(Math.random() * numbers.length);
+        var item = numbers[numberRandomIndex];
+        // console.log(item);
+        suggestedPassword.push(item);
     }
-    for (var i = 0; i < randomNumType2; i++) {
-        suggestedPassword.push('2');
+    for (var i = 0; i < randomIteration2; i++) {
+        var specialCharRandomIndex = Math.floor(
+            Math.random() * specialChar.length
+        );
+        var item2 = specialChar[specialCharRandomIndex];
+        // console.log(item2);
+        suggestedPassword.push(item2);
     }
-    for (var i = 0; i < randomNumType3; i++) {
-        suggestedPassword.push('3');
+    for (var i = 0; i < randomIteration3; i++) {
+        var uppercaseRandomIndex = Math.floor(Math.random() * uppercase.length);
+        var item3 = uppercase[uppercaseRandomIndex];
+        // console.log(item3);
+        suggestedPassword.push(item3);
     }
-    for (var i = 0; i < randomNumType4; i++) {
-        suggestedPassword.push('4');
+    for (var i = 0; i < randomIteration4; i++) {
+        var lowercaseRandomIndex = Math.floor(Math.random() * lowercase.length);
+        var item4 = lowercase[lowercaseRandomIndex];
+        // console.log(item4);
+        suggestedPassword.push(item4);
     }
-
-    console.log(suggestedPassword.toString());
+    // console.log('*****************');
+    // console.log(suggestedPassword.toString());
+    return suggestedPassword.join('').toString();
 }
 
-passwordGenerator(numbers, specialChar, uppercase, lowercase);
-// generate a random number based on length of array + 2
+function showNewPassword() {
+    var finalPassword = passwordGenerator(
+        numbers,
+        specialChar,
+        uppercase,
+        lowercase
+    );
+    placeholderText.value = finalPassword;
+}
 
+generatePasswordBtn.addEventListener('click', showNewPassword);
+
+///working code above ^^^^^^
+
+var copy = function copyToClipboard() {
+    var newPassword = placeholderText.getAttribute('placeholder');
+
+    console.log(newPassword);
+};
+
+copyToClipboardBtn.addEventListener('click', copy);
+
+// var copy = function copy() {
+//     var copyText = document.querySelector('#password');
+//     copyText.select();
+//     document.execCommand('copy');
+// };
+
+// copyToClipboardBtn.addEventListener('click', copy);
+
+//
+//
+//
+//
+//
 ////////////////// function to generate array of all letters.
 // const letters = (() => {
 //     const caps = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
 //     return caps.concat(caps.map(letter => letter.toLowerCase()));
 // })();
 // console.log(letters.join());
-
-//////////////////BUTTON TEST
-
-// copyToClipboard.addEventListener('click', myFunction);
-
-// function myFunction() {
-//     console.log('Hello World!');
-// }
